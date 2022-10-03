1052ef9b62db42c1cc0c8de28ac393c459468d7f
CodeRubbish <1083926298@qq.com>
2022-10-03 09:38:21 +0800

    init

--- monorepo-tools/bin/index.js
+++ monorepo-tools/bin/index.js
@@ -1,2 +1,11 @@
 #!/usr/bin/env node
-console.log('1');
+'use strict';
+
+var commander = require('commander');
+
+var version = "1.0.0";
+
+commander.program
+    .name('monorepo-tools')
+    .description('CLI to run monorepo for react module federation project')
+    .version(version);
